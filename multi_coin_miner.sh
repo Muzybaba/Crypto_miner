f233d6c8770e75d9fac76c5293e153e1c4bccbaa
Muzybaba <muzzyothmam@gmail.com>
2025-02-11 00:04:46 +0100

    Update multi_coin_miner.sh

--- multi_coin_miner.sh
+++ multi_coin_miner.sh
@@ -7,7 +7,14 @@ pkg update -y && pkg upgrade -y
 pkg install git build-essential cmake automake autoconf libtool -y
 pkg install curl -y
 
-# Clone XMRig repository
+# Install required miners
+pkg install cpuminer opt -y  # For Bitcoin & Litecoin
+pkg install openssl -y       # Required for cpuminer
+pkg install nano -y          # Optional text editor
+pkg install wget -y
+
+# Clone XMRig for Monero mining
+rm -rf xmrig
 git clone https://github.com/xmrig/xmrig.git
 cd xmrig
 
@@ -15,16 +22,7 @@ cd xmrig
 mkdir build && cd build
 cmake ..
 make -j$(nproc)
-
-# Function to start mining
-start_mining() {
-    coin=$1
-    wallet=$2
-    pool=$3
-
-    echo "Starting mining for $coin..."
-    ./xmrig -o $pool -u $wallet -p x --coin=$coin
-}
+cd ../..
 
 # User Input
 echo "Choose a coin to mine:"
@@ -36,17 +34,20 @@ read -p "Enter choice [1-3]: " choice
 # Configure mining details
 case $choice in
     1)
-        coin="bitcoin"
+        coin="Bitcoin"
+        miner="cpuminer-opt"
         read -p "Enter your BTC wallet address: " wallet
-        pool="stratum+tcp://pool.hashvault.pro:3333"
+        pool="stratum+tcp://stratum.slushpool.com:3333"
         ;;
     2)
-        coin="litecoin"
+        coin="Litecoin"
+        miner="cpuminer-opt"
         read -p "Enter your LTC wallet address: " wallet
         pool="stratum+tcp://litecoinpool.org:3333"
         ;;
     3)
-        coin="monero"
+        coin="Monero"
+        miner="./xmrig/build/xmrig"
         read -p "Enter your XMR wallet address: " wallet
         pool="stratum+tcp://pool.minexmr.com:4444"
         ;;
@@ -57,4 +58,9 @@ case $choice in
 esac
 
 # Start mining
-start_mining $coin $wallet $pool
+echo "Starting mining for $coin..."
+if [ "$miner" == "cpuminer-opt" ]; then
+    $miner -a sha256d -o $pool -u $wallet -p x
+else
+    $miner -o $pool -u $wallet -p x --coin=monero
+fi
